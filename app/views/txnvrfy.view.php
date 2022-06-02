6a3769bfbad9534c642b54d33b93024b8c22f5b6
Paul M. Foster <paulf@quillandmouse.com>
2022-06-01 22:01:02 -0400

    Other part of add transaction bug fix-- the view file.

---
+++ app/views/txnvrfy.view.php
@@ -0,0 +1,180 @@
+<?php include VIEWDIR . 'head.view.php' ?>
+
+<?php extract($data); ?>
+
+<form method="post" action="<?php echo $this->return; ?>">
+
+<?php $row = 0; ?>
+<?php $p = 0; ?>
+
+<?php foreach ($txns as $txn): ?>
+
+<?php if ($p > 0): ?>
+<h3>Transfer Information</h3>
+<?php endif; ?>
+
+<table>
+<tr><th>Item</th><th>Value</th></tr>
+
+<tr class="row<?php echo $row++ & 1; ?>">
+<td>
+<label>Transaction ID</label>
+</td>
+<td>
+<?php echo $txn['txnid']; ?>
+</td>
+</tr>
+
+<tr class="row<?php echo $row++ & 1; ?>">
+<td>
+<label>From Account</label>
+</td>
+<td>
+<?php echo $txn['from_acct'] . ' ' . $txn['from_acct_name']; ?>
+</td>
+</tr>
+
+<tr class="row<?php echo $row++ & 1; ?>">
+<td>
+<label>Date</label>
+</td>
+<td>
+<?php echo pdate::iso2am($txn['txn_dt']); ?>
+</td>
+</tr>
+
+<tr class="row<?php echo $row++ & 1; ?>">
+<td>
+<label>Check #</label>
+</td>
+<td>
+<?php echo $txn['checkno']; ?>
+</td>
+</tr>
+
+<tr class="row<?php echo $row++ & 1; ?>">
+<td>
+<label>Split?</label>
+</td>
+<td>
+<?php echo $txn['split'] ? 'Yes' : 'No';?>
+</td>
+</tr>
+
+<tr class="row<?php echo $row++ & 1; ?>">
+<td>
+<label>Payee</label>
+</td>
+<td>
+<?php echo $txn['payee_id'] . ' ' . $txn['payee_name']; ?>
+</td>
+</tr>
+
+<tr class="row<?php echo $row++ & 1; ?>">
+<td>
+<label>To Account</label>
+</td>
+<td>
+<?php echo $txn['to_acct'] . ' ' .  $txn['to_acct_name']; ?>
+</td>
+</tr>
+
+<tr class="row<?php echo $row++ & 1; ?>">
+<td>
+<label>Memo</label>
+</td>
+<td>
+<?php echo $txn['memo']; ?>
+</td>
+</tr>
+
+<tr class="row<?php echo $row++ & 1; ?>">
+<td>
+<label>Status</label>
+</td>
+<td>
+<?php echo $txn['x_status']; ?>
+</td>
+</tr>
+
+<tr class="row<?php echo $row++ & 1; ?>">
+<td>
+<label>Reconciliation Date</label>
+</td>
+<td>
+<?php echo pdate::iso2am($txn['recon_dt']); ?>
+</td>
+</tr>
+
+<tr class="row<?php echo $row++ & 1; ?>">
+<td>
+<label>Amount</label>
+</td>
+<td>
+<?php if ($txn['amount'] < 0) {
+	echo $txn['dr_amount'];
+}
+else {
+	echo $txn['cr_amount'];
+}
+?>
+
+</table>
+
+<?php if ($txn['split'] == 1): ?>
+<?php $splitno = 1; ?>
+<h3>Splits</h3>
+<table rules="all" border="1">
+<tr><th>#</th><th>Item</th><th>Value</th></tr>
+<?php foreach ($splits as $split): ?>
+
+<tr>
+<td rowspan="4"><?php echo $splitno; ?></td>
+<td><label>Payee</label></td>
+<td>
+<?php echo $split['payee_id'] . ' ' . $split['payee_name']; ?>
+</td>
+</tr>
+
+<tr>
+<td><label>Destination Acct</label></td>
+<td>
+<?php echo $split['to_acct'] . ' ' . $split['to_acct_name']; ?>
+</td>
+</tr>
+
+<tr>
+<td><label>Memo</label></td>
+<td>
+<?php echo $split['memo']; ?>
+</td>
+</tr>
+
+<tr>
+<td><label>Amount</label></td>
+<td>
+<?php echo int2dec($split['amount']); ?>
+</td>
+</tr>
+
+<?php $splitno++; ?>
+
+<?php endforeach; ?>
+</table>
+<?php endif; /* has splits */ ?>
+
+<?php $p++; ?>
+
+<?php endforeach; ?>
+
+<p>
+<?php $this->form->hidden('txnid'); ?>
+<?php $this->form->submit('confirm'); ?>
+&nbsp;
+<?php form::abandon("index.php"); ?>
+</p>
+
+</form>
+
+<?php include VIEWDIR . 'footer.view.php'; ?>
+
