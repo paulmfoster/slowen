67d6182a9c1bad6767dcf735a75827a948779234
Paul M. Foster <paulf@quillandmouse.com>
2022-06-05 02:04:16 -0400

    Fix bug in expenses report of report model. Hopefully DONE.

--- app/models/report.php
+++ app/models/report.php
@@ -207,41 +207,23 @@ class report
 	
 	function get_expenses($from_date, $to_date)
 	{
-        $sql = "select j.*, 
-            a.name as from_acct_name, 
-            b.name as to_acct_name, 
-            p.name as payee_name 
-            from journal as j 
-            join accounts as a on (a.id = j.from_acct) 
-            join accounts as b on (b.id = j.to_acct) 
-            join payees as p on (p.id = j.payee_id) 
-            where b.acct_type = 'E' 
-            and j.txn_dt >= '$from_date' 
-            and j.txn_dt <= '$to_date' 
-            order by to_acct_name";
+        $sql = "select journal.*, 
+        a.name as from_acct_name, 
+        b.name as to_acct_name, 
+        payees.name as payee_name from journal 
+        join accounts as a on a.id = journal.from_acct 
+        join accounts as b on b.id = journal.to_acct 
+        join payees on payees.id = journal.payee_id
+        where b.acct_type = 'E' 
+        and journal.txn_dt >= '$from_date' 
+        and journal.txn_dt <= '$to_date' 
+        order by to_acct_name";
+
 		$result = $this->db->query($sql)->fetch_all();
-        instrument('result', $result);
-        die('golf');
+
         if ($result === FALSE) {
             return FALSE;
         }
-
-        /*
-        // fixme: is the following needed?
-		// add from_acct name
-		$sql = "SELECT id, name FROM accounts WHERE acct_type in ('C', 'R', 'S')";
-		$accts = $this->db->query($sql)->fetch_all();
-
-		$nres = count($result);
-		for ($i = 0; $i < $nres; $i++) {
-			foreach ($accts as $acct) {
-				if ($result[$i]['from_acct'] == $acct['id']) {
-					$result[$i]['fromname'] = $acct['name'];
-					break;
-				}
-			}
-		}
-         */
 
 		return $result;
 	}
