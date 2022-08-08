9e63a66e34cd23d1dcbe9b2b90cde68882f98982
Paul M. Foster <paulf@quillandmouse.com>
2022-08-07 21:01:04 -0400

    Add expenses and payments for use with external programs.

---
+++ expenses.php
@@ -0,0 +1,48 @@
+<?php
+
+// This is designed to be called remotely and provide the expenditures for
+// the week, according to Slowen. It's used for the Budget application.
+
+// define system directories
+define('SYSDIR', 'system/');
+define('INCDIR', SYSDIR . 'includes/');
+define('LIBDIR', SYSDIR . 'libraries/');
+
+// define application directories
+define('APPDIR', 'app/');
+define('CFGDIR', APPDIR . 'config/');
+define('DATADIR', APPDIR . 'data/');
+
+// provide common utilities
+include INCDIR . 'utils.inc.php';
+
+$cfg = parse_ini_file(CFGDIR . 'config.ini');
+$db = load('database', $cfg['dsn']);
+
+$to_date = $_GET['to'] ?? NULL;
+$from_date = $_GET['from'] ?? NULL;
+
+if (is_null($from_date) || is_null($to_date)) {
+    exit('');
+}
+
+$sql = "select journal.*, 
+a.name as from_acct_name, 
+b.name as to_acct_name, 
+payees.name as payee_name from journal 
+join accounts as a on a.id = journal.from_acct 
+join accounts as b on b.id = journal.to_acct 
+join payees on payees.id = journal.payee_id
+where b.acct_type = 'E' 
+and journal.txn_dt >= '$from_date' 
+and journal.txn_dt <= '$to_date' 
+order by to_acct_name";
+
+$result = $db->query($sql)->fetch_all();
+if ($result === FALSE) {
+    exit('');
+}
+
+$json = json_encode($result);
+echo $json;
+
---
+++ payments.php
@@ -0,0 +1,49 @@
+<?php
+
+// This is designed to be called remotely and provide the payments to
+// credit card and credit accounts. For use by the Budget app.
+
+// define system directories
+define('SYSDIR', 'system/');
+define('INCDIR', SYSDIR . 'includes/');
+define('LIBDIR', SYSDIR . 'libraries/');
+
+// define application directories
+define('APPDIR', 'app/');
+define('CFGDIR', APPDIR . 'config/');
+define('DATADIR', APPDIR . 'data/');
+
+// provide common utilities
+include INCDIR . 'utils.inc.php';
+
+$cfg = parse_ini_file(CFGDIR . 'config.ini');
+$db = load('database', $cfg['dsn']);
+
+$to_date = $_GET['to'] ?? NULL;
+$from_date = $_GET['from'] ?? NULL;
+
+if (is_null($from_date) || is_null($to_date)) {
+    exit('');
+}
+
+// transactions:
+// where txn_dt between from and to dates
+// where to_acct is a credit card
+// where transaction is a credit
+
+$sql = "select journal.* from journal, accounts 
+where txn_dt >= '$from_date' 
+and txn_dt <= '$to_date' 
+and amount > 0 
+and journal.from_acct = accounts.id 
+and accounts.acct_type = 'R'";
+
+$result = $db->query($sql)->fetch_all();
+
+if ($result === FALSE) {
+    exit('');
+}
+
+$json = json_encode($result);
+echo $json;
+
